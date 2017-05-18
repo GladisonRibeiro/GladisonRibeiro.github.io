aa4fa6d950c9aa592781d3b6ec2089ab2d6b4e83
gladison ribeiro da silva <gladison ribeiro da silva>
2017-05-18 08:20:55 -0300

    Incluindo service-worker e manifest, editando estilo

--- js/app.js
+++ js/app.js
@@ -1,62 +1,71 @@
 angular.module('politicard', ['ui.router']);
 
-angular.module('politicard').config(function($stateProvider, $urlRouterProvider) {
+angular.module('politicard').run(function () {
     
+    if ('serviceWorker' in navigator) {
+        navigator.serviceWorker
+            .register('./service-worker.js')
+            .then(function () { console.log('Service Worker Registrado'); });
+    }
+});
+
+angular.module('politicard').config(function ($stateProvider, $urlRouterProvider) {
+
     $urlRouterProvider.otherwise('/splashscreen');
-    
+
     $stateProvider
-        
+
         .state('splashscreen', {
             url: '/splashscreen',
             templateUrl: '../templates/splashscreen.html',
-            controller: function($state) {
-                setTimeout(function(){
-                   $state.go('menu'); 
-                },3500);
+            controller: function ($state) {
+                setTimeout(function () {
+                    $state.go('menu');
+                }, 3500);
             }
         })
-        
+
         .state('menu', {
             url: '/menu',
             templateUrl: '../templates/menu.html',
-            controller: function($scope) {
-                
+            controller: function ($scope) {
+
             }
         })
-    
+
         .state('arena', {
             url: '/arena',
             templateUrl: '../templates/arena.html',
             controller: 'ArenaController'
         })
-    
+
         .state('instrucao', {
             url: '/instrucao',
             templateUrl: '../templates/instrucao.html',
         })
-    
+
         .state('creditos', {
             url: '/creditos',
             templateUrl: '../templates/creditos.html',
-            controller: function($scope) {
-                $scope.equipe = [{nome:'Cleber'}, {nome:'Gladison'}, {nome:'Jefferson'}, {nome:'Keoma'}, {nome:'Pedro'}]
+            controller: function ($scope) {
+                $scope.equipe = [{ nome: 'Cleber' }, { nome: 'Gladison' }, { nome: 'Jefferson' }, { nome: 'Keoma' }, { nome: 'Pedro' }]
             }
         })
-        
+
         .state('galeria', {
             url: '/galeria',
             templateUrl: '../templates/galeria.html',
             controller: 'GaleriaController',
             controllerAs: "vm"
         })
-        
+
         .state('resultado', {
             url: '/resultado/:resultado',
             templateUrl: '../templates/resultado.html',
-            controller: function($scope, $stateParams) {
+            controller: function ($scope, $stateParams) {
                 $scope.resultado = $stateParams.resultado;
             }
         });
-    
-        
+
+
 });
